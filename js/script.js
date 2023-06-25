89b28e5521f5b2d58ba8aa8e7136ff17f595b25a
angelarenata <angelarenata.a@hotmail.com>
2023-06-25 14:57:36 -0300

    Criação dos script

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const adicionar = document.querySelector("#adicionar");
+const adicionarInput = document.querySelector("#adicionar-input");
+
+// Função
+const salvar = (text)  => {
+    
+}
+
+// Eventos
+
+adicionar.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const valorInput = adicionarInput.ariaValueMax;
+
+    if (valorInput) {
+
+    }
+});
